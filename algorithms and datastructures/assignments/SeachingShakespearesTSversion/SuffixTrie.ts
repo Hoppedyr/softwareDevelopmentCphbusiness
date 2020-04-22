becc4e7319283951aa4857c08402380118d13e98
Rasmus Jarnborg Friis <rasmusjf@hotmail.com>
2020-04-22 15:57:47 +0200

    Added seachingShake

---
+++ algorithms and datastructures/assignments/SeachingShakespearesTSversion/SuffixTrie.ts
@@ -0,0 +1,49 @@
+import { MyNode } from './MyNode';
+
+export class SuffixTrie {
+  private nodes = new Array<MyNode>();
+
+  public add(word: string): void {
+    // makes word lowercase
+    word = word.toLowerCase();
+    // makes sure we use all suffixes of the word
+    for (let i = 0; i < word.length; i++) {
+      // Create a substring for each suffix
+      let substring: string = word;
+      // if the word is longer then 1 subtract the the first letter
+      if (word.length != 1) {
+        substring = word.substring(i);
+      }
+      // Takes the first letter in our substring and saves it in letter
+      let letter: string = substring.charAt(0);
+
+      // Iterates each child node and see if we can find the node with the same letter
+      // we then call that nodes addChild method
+      let found: boolean = false;
+      for (const node of this.nodes) {
+        if (node.getLetter() == letter) {
+          node.addChild(substring, word);
+          found = true;
+        }
+      }
+      // If we cant find that node we create a new node with the given letter
+      if (!found) {
+        let newNode = new MyNode(letter, 1);
+        this.nodes.push(newNode);
+        newNode.addChild(substring, word);
+      }
+    }
+  }
+
+  public find(substring: string): Set<string> {
+    // We find the first letter and checks if we can find a node with the given
+    // letter in out child list
+    let rootLetter: string = substring.charAt(0);
+    for (const node of this.nodes) {
+      if (node.getLetter() == rootLetter) {
+        return node.find(substring, substring);
+      }
+    }
+    return new Set()
+  }
+}
---
+++ algorithms and datastructures/assignments/SeachingShakespearesTSversion/main.ts
@@ -0,0 +1,42 @@
+import { SuffixTrie } from './SuffixTrie';
+import * as readline from 'readline';
+import * as fs from 'fs';
+
+let rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+const st: SuffixTrie = new SuffixTrie();
+
+
+function setUp(): void {
+  let text: string = fs.readFileSync("./text/shakespeare-complete-works.txt").toString('utf-8');
+  let textcleaned: string = text.replace(/[^\w\s]|_/g, " ").replace(/\s+/g, " ").toLowerCase();
+  const textArray: Array<string> = textcleaned.split(' ');
+  textArray.forEach(word => {
+    st.add(word);
+  })
+  console.log(textArray.length)
+};
+
+function start(): void {
+  rl.question('Enter a substring to find matching words: \n', (substring) => {
+    console.time();
+    let res: Set<string> = st.find(substring);
+    console.log('\n')
+    if (res != undefined)
+      for (const item of Array.from(res.values())) {
+        console.log(item);
+      } else {
+      console.log('could not find any matching words:' + " " + substring)
+    }
+    console.timeEnd();
+    start()
+  });
+};
+
+
+
+setUp()
+start();
+
